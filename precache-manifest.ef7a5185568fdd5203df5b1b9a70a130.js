388408f64752b8c2bd7a22ab094b506cbb8feec3
Jhoan Alejandro Anaya <jaanaya88@misena.edu.co>
2020-03-01 21:23:16 -0500

    Updates

---
+++ precache-manifest.ef7a5185568fdd5203df5b1b9a70a130.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea87a90c3bbe45b78714d64e62379dd",
+    "url": "/appsus/index.html"
+  },
+  {
+    "revision": "32ea19aafe6ae4616f92",
+    "url": "/appsus/static/css/main.79914235.chunk.css"
+  },
+  {
+    "revision": "5484559a18bf11852833",
+    "url": "/appsus/static/js/2.db33e80d.chunk.js"
+  },
+  {
+    "revision": "35e9035c4541049d41232fe0bf96f70b",
+    "url": "/appsus/static/js/2.db33e80d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32ea19aafe6ae4616f92",
+    "url": "/appsus/static/js/main.8c2278bb.chunk.js"
+  },
+  {
+    "revision": "925b1750e56cd011990d",
+    "url": "/appsus/static/js/runtime-main.92f648f7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/appsus/static/media/logo.5d5d9eef.svg"
+  }
+]);
